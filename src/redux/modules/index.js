7561f2aa1b30c3d598258afa0a3cc80491861420
TDoraX <zhangze612@163.com>
2019-04-09 16:43:34 +0800

    State Module Init

---
+++ src/redux/modules/index.js
@@ -0,0 +1,13 @@
+import { combineReducer } from 'redux';
+import entities from './entities';
+import home from './home';
+import detail from './detail';
+import app from './app';
+
+// 合并Root Reducer
+const rootReducer = combineReducer({
+    entities,
+    home,
+    detail,
+    app
+});
