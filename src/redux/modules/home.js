4f104b53ba0d074098bea41bec47aadf18306d4b
TDoraX <zhangze612@163.com>
2019-04-11 14:43:14 +0800

    Module Home Actions Added

--- src/redux/modules/home.js
+++ src/redux/modules/home.js
@@ -3,10 +3,21 @@ import url from '../../utils/url';
 import {FETCH_DATA} from "../middleware/api";
 import {schema} from './entities/products';
 
+// 请求参数使用到的常量对象
+export const params = {
+    PATH_LIKES: 'likes',
+    PATH_DISCOUNTS: 'discounts',
+    PAGE_SIZE_LIKES: 5,
+    PAGE_SIZE_DISCOUNTS: 3
+};
+
 export const types = {
     FETCH_LIKES_REQUEST: 'HOME/FETCH_LIKES_REQUEST', // 获取“猜你喜欢”请求
     FETCH_LIKES_SUCCESS: 'HOME/FETCH_LIKES_SUCCESS', // 获取“猜你喜欢”请求成功
     FETCH_LIKES_FAILURE: 'HOME/FETCH_LIKES_FAILURE', // 获取“猜你喜欢”请求失败
+    FETCH_DISCOUNT_REQUEST: 'HOME/FETCH_DISCOUNT_REQUEST', // 获取“特惠信息”请求
+    FETCH_DISCOUNT_SUCCESS: 'HOME/FETCH_DISCOUNT_SUCCESS', // 获取“特惠信息”请求成功
+    FETCH_DISCOUNT_FAILURE: 'HOME/FETCH_DISCOUNT_FAILURE', // 获取“特惠信息”请求失败
 };
 
 const initialState = {
@@ -22,10 +33,21 @@ const initialState = {
 };
 
 export const actions = {
+    // 加载“猜你喜欢”的数据
     loadLikes: () => {
         return (dispatch, getState) => {
-            const endpoint = url.getProductList(0, 10);
+            const {pageCount} = getState().home.likes;
+            const rowIndex = pageCount * params.PAGE_SIZE_LIKES;
+            const endpoint = url.getProductList(params.PATH_LIKES, rowIndex, params.PAGE_SIZE_LIKES);
             return dispatch(fetchLikes(endpoint));
+        }
+    },
+
+    // 加载“特惠信息”的数据
+    loadDiscounts: () => {
+        return (dispatch, getState) => {
+            const endpoint = url.getProductList(params.PAGE_SIZE_DISCOUNTS, 0, params.PAGE_SIZE_DISCOUNTS);
+            return dispatch(fetchDiscounts(endpoint));
         }
     }
 };
@@ -36,6 +58,18 @@ const fetchLikes = (endpoint) => ({
             types.FETCH_LIKES_REQUEST,
             types.FETCH_LIKES_SUCCESS,
             types.FETCH_LIKES_FAILURE
+        ],
+        endpoint,
+        schema
+    }
+});
+
+const fetchDiscounts = (endpoint) => ({
+    [FETCH_DATA]: {
+        types: [
+            types.FETCH_DISCOUNT_REQUEST,
+            types.FETCH_DISCOUNT_SUCCESS,
+            types.FETCH_DISCOUNT_FAILURE
         ],
         endpoint,
         schema
