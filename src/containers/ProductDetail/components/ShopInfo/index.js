4b2b26f87e0d62a9836838bfb8622a8c7c6a30cd
TDoraX <zhangze612@163.com>
2019-05-07 16:43:02 +0800

    Module Detail Complete

--- src/containers/ProductDetail/components/ShopInfo/index.js
+++ src/containers/ProductDetail/components/ShopInfo/index.js
@@ -3,28 +3,34 @@ import './style.css';
 
 class ShopInfo extends Component {
     render() {
+        const {data: relatedShop, total} = this.props;
+        const {shop: name, star, address, phone} = relatedShop;
         return (
             <div className="shopInfo">
                 <div className="shopInfo__header">
-                    使用商户（4）
-                    <span className="shopInfo__arrow"></span>
+                    使用商户（{total}）
+                    <span className="shopInfo__arrow"/>
                 </div>
                 <div className="shopInfo__middle">
                     <div className="shopInfo__middleLeft">
-                        <div className="shopInfo__shopName">院落创意菜</div>
+                        <div className="shopInfo__shopName">{name}</div>
                         <div className="shopInfo__starsWrapper">
-                            <span className="shopInfo__stars">
-                                <i className="shopInfo__stars--red" style={{"width": "80%"}}/>
-                            </span>
+              <span className="shopInfo__stars">
+                <i
+                    className="shopInfo__stars--red"
+                    style={{width: 2 * star + "%"}}
+                />
+              </span>
                             <span className="shopInfo__distance">>100km</span>
                         </div>
                     </div>
-                    <div className="shopInfo__middleRight">
+                    <a className="shopInfo__middleRight" href={`tel://${phone}`}>
                         <i className="shopInfo__phoneIcon"/>
-                    </div>
+                    </a>
                 </div>
                 <div className="shopInfo__bottom">
-                    <i className="shopInfo__locationIcon"/>杭州滨江区
+                    <i className="shopInfo__locationIcon"/>
+                    {address}
                 </div>
             </div>
         );
--- src/redux/modules/entities/shops.js
+++ src/redux/modules/entities/shops.js
@@ -1,5 +1,15 @@
-const reducer = (state = {}, action) => {
-    return state;
+import createReducer from '../../../utils/createReducer';
+
+export const schema = {
+    name: 'shops',
+    id: 'id'
 };
 
+const reducer = createReducer(schema.name);
+
 export default reducer;
+
+// selectors
+export const getShopById = (state, id) => {
+    return state.entities.shops[id];
+};
